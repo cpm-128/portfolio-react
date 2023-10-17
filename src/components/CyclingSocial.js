1a4302abed31476dadffd269e306694d05db145b
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2023-10-16 21:25:10 -0400

    added athlinks social icon and link

--- src/components/CyclingSocial.js
+++ src/components/CyclingSocial.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import stravaLogo from './../assets/images/logos/strava-logo.png';
 import instagramLogo from './../assets/images/logos/instagram-logo.png';
+import athlinksLogo from './../assets/images/logos/athlinks-logo.jpeg';
 
 function CyclingSocial() {
     return(
         <div className='bg-color-tertiary-opaque mt-auto padding-topbottom-20px'>
 
         <div className='d-flex flex-row justify-content-center'>
+
+            {/* ATHLINKS */}
+            <div className='p-2 social-icon'>
+                <a href='https://www.athlinks.com/athletes/498448960/results' target='_blank' rel='noopener noreferrer'>
+                    <img src={athlinksLogo} alt='Athlinks logo linked to profile' />
+                </a>
+            </div>
 
             {/* STRAVA */}
             <div className='p-2 social-icon'>
